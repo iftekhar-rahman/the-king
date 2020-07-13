bf1ceee156206219b00daccd77b092008964077a
Iftekhar Rahman <iftekharmail1@gmail.com>
2020-07-12 21:19:14 -0700

    isotope update

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,99 +23,6 @@ jQuery(document).ready(function($){
 
     $('select').niceSelect();
 
-    // homepage-slides
-	// $(".homepage-slides").owlCarousel({
-	// 	items: 1,
-	// 	loop: true,
-	// 	nav: true,
-	// 	dots: false,
-    //     autoplay: false,
-    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
-	// });
-
-    // $("#range_49").ionRangeSlider({
-    //     type: "single",
-    //     min: 150,
-    //     max: 250,
-    //     from: 200,
-    //     to: 250,
-    //     prefix: "£",
-    //     step: 1,
-    //     onFinish: function(data) {
-    //         maxValue = data.from;
-    //         alert("Your Value: " + maxValue);
-    //     }
-    // });
-
-
-    // $(".logo-carousel-wrap").owlCarousel({
-    //     items: 5,
-    //     loop: true,
-    //     nav: false,
-    //     dots: false,
-    //     autoplay: false,
-    //     margin: 15,
-    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-    //     responsive: {
-    //         0: {
-    //             items: 2,
-    //             margin: 15,
-    //             nav: false
-    //         },
-    //         580: {
-    //             items: 3,
-    //             margin: 3,
-    //             nav: false
-    //         },
-    //         720: {
-    //             items: 4,
-    //             margin: 3,
-    //             nav: false
-    //         },
-    //         960: {
-    //             items: 5
-    //         },
-    //         1140: {
-    //             items: 6
-    //         }
-
-    //     }
-    // });
-
-    // $(".feature-carousel-wrap").owlCarousel({
-    //     items: 4,
-    //     loop: true,
-    //     nav: true,
-    //     dots: true,
-    //     autoplay: false,
-    //     margin: 15,
-    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-    //     responsive: {
-    //         0: {
-    //             items: 1,
-    //             margin: 0,
-    //             nav: false
-    //         },
-    //         481: {
-    //             items: 2,
-    //             margin: 15,
-    //             nav: false
-    //         },
-    //         720: {
-    //             items: 3,
-    //             margin: 15,
-    //             nav: false
-    //         },
-    //         960: {
-    //             items: 3,
-    //             margin: 25,
-    //         },
-    //         1140: {
-    //             items: 4
-    //         }
-
-    //     }
-    // });
 
     $(".single-accordion").on("click", function(){
         $(".single-accordion").removeClass("active");
@@ -214,57 +121,72 @@ jQuery(document).ready(function($){
 
     // portfolio isotope js
     // init Isotope
-    var $grid = $('.grid').isotope({
-        itemSelector: '.single-portfolio-item'
-    });
-    var $filterButtons = $('.filters .button');
-    updateFilterCounts();
+    // var $grid = $('.grid').isotope({
+    //     itemSelector: '.single-portfolio-item'
+    // });
+    // var $filterButtons = $('.filters .button');
+    // updateFilterCounts();
     // store filter for each group
-    var filters = {};
-    $('.filters').on( 'click', '.button', function() {
-        var $this = $(this);
-        // get group key
-        var $buttonGroup = $this.parents('.button-group');
-        var filterGroup = $buttonGroup.attr('data-filter-group');
-        // set filter for group
-        filters[ filterGroup ] = $this.attr('data-filter');
-        // combine filters
-        var filterValue = concatValues( filters );
-        // set filter for Isotope
-        $grid.isotope({ filter: filterValue });
-        updateFilterCounts();
-    });
+    // var filters = {};
+    // $('.filters').on( 'click', '.button', function() {
+    //     var $this = $(this);
+    //     // get group key
+    //     var $buttonGroup = $this.parents('.button-group');
+    //     var filterGroup = $buttonGroup.attr('data-filter-group');
+    //     // set filter for group
+    //     filters[ filterGroup ] = $this.attr('data-filter');
+    //     // combine filters
+    //     var filterValue = concatValues( filters );
+    //     // set filter for Isotope
+    //     $grid.isotope({ filter: filterValue });
+    //     updateFilterCounts();
+    // });
     // change is-checked class on buttons
-    $('.button-group').each( function( i, buttonGroup ) {
-        var $buttonGroup = $( buttonGroup );
-        $buttonGroup.on( 'click', 'button', function() {
-            $buttonGroup.find('.is-checked').removeClass('is-checked');
-            $( this ).addClass('is-checked');
-        });
-    });
+    // $('.button-group').each( function( i, buttonGroup ) {
+    //     var $buttonGroup = $( buttonGroup );
+    //     $buttonGroup.on( 'click', 'button', function() {
+    //         $buttonGroup.find('.is-checked').removeClass('is-checked');
+    //         $( this ).addClass('is-checked');
+    //     });
+    // });
     // flatten object by concatting values
-    function concatValues( obj ) {
-        var value = '';
-        for ( var prop in obj ) {
-            value += obj[ prop ];
-        }
-        return value;
-    }
-    function updateFilterCounts()  {
-        // get filtered item elements
-        var itemElems = $grid.isotope('getFilteredItemElements');
-        var $itemElems = $( itemElems );
-        $filterButtons.each( function( i, button ) {
-            var $button = $( button );
-            var filterValue = $button.attr('data-filter');
-            if ( !filterValue ) {
-                // do not update 'any' buttons
-                return;
-            }
-            var count = $itemElems.filter( filterValue ).length;
-            $button.find('.filter-count').text( '(' + count +')' );
+    // function concatValues( obj ) {
+    //     var value = '';
+    //     for ( var prop in obj ) {
+    //         value += obj[ prop ];
+    //     }
+    //     return value;
+    // }
+    // function updateFilterCounts()  {
+    //     // get filtered item elements
+    //     var itemElems = $grid.isotope('getFilteredItemElements');
+    //     var $itemElems = $( itemElems );
+    //     $filterButtons.each( function( i, button ) {
+    //         var $button = $( button );
+    //         var filterValue = $button.attr('data-filter');
+    //         if ( !filterValue ) {
+    //             // do not update 'any' buttons
+    //             return;
+    //         }
+    //         var count = $itemElems.filter( filterValue ).length;
+    //         $button.find('.filter-count').text( '(' + count +')' );
+    //     });
+    // }
+
+    //jQuery for portfolio section
+    $(".portfolio-filter button").on('click', function(){
+
+        $(".portfolio-filter button").removeClass("is-checked");
+        $(this).addClass("is-checked");
+
+
+        var selector =  $(this).attr("data-filter");
+
+        $(".grid").isotope({
+            filter: selector,
         });
-    }
+
+    });
   
 
     // Services page

42ecd236240009d8d29dbe9cd095b9865f5d987d
Iftekhar Rahman <iftekharmail1@gmail.com>
2020-07-17 21:24:41 -0700

    ddd

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,69 +90,69 @@ jQuery(document).ready(function($){
     }); // click() scroll top ENDS
 
     /* portfolio isotope js */
-    var $grid = $('.grid').isotope({
-        itemSelector: '.single-portfolio-item'
-    });
-    var $filterButtons = $('.filters .button');
-    updateFilterCounts();
-    /* store filter for each group*/
-    var filters = {};
-    $('.filters').on( 'click', '.button', function() {
-        var $this = $(this);
-        var $buttonGroup = $this.parents('.button-group');
-        var filterGroup = $buttonGroup.attr('data-filter-group');
-        filters[ filterGroup ] = $this.attr('data-filter');
-        var filterValue = concatValues( filters );
-        $grid.isotope({ filter: filterValue });
-        updateFilterCounts();
-    });
-    /* change is-checked class on buttons*/
-    $('.button-group').each( function( i, buttonGroup ) {
-        var $buttonGroup = $( buttonGroup );
-        $buttonGroup.on( 'click', 'button', function() {
-            $buttonGroup.find('.is-checked').removeClass('is-checked');
-            $( this ).addClass('is-checked');
-        });
-    });
-    /* flatten object by concatting values */
-    function concatValues( obj ) {
-        var value = '';
-        for ( var prop in obj ) {
-            value += obj[ prop ];
-        }
-        return value;
-    }
-    function updateFilterCounts()  {
-        var itemElems = $grid.isotope('getFilteredItemElements');
-        var $itemElems = $( itemElems );
-        $filterButtons.each( function( i, button ) {
-            var $button = $( button );
-            var filterValue = $button.attr('data-filter');
-            if ( !filterValue ) {
-                return;
-            }
-            var count = $itemElems.filter( filterValue ).length;
-            $button.find('.filter-count').text( '(' + count +')' );
-        });
-    }
-
-    // $('.filters button').click(function(){
-    //     $('.filters button').removeClass('is-checked');
-    //     $(this).addClass('is-checked');
-        
-    //     var data = $(this).attr('data-filter');
-    //     $grid.isotope({
-    //       filter: data
-    //     })
+    // var $grid = $('.grid').isotope({
+    //     itemSelector: '.single-portfolio-item'
     // });
-      
-    // var $grid = $(".grid").isotope({
-    //     itemSelector: ".all",
-    //     percentPosition: true,
-    //     masonry: {
-    //         columnWidth: ".all"
+    // var $filterButtons = $('.filters .button');
+    // updateFilterCounts();
+    // /* store filter for each group*/
+    // var filters = {};
+    // $('.filters').on( 'click', '.button', function() {
+    //     var $this = $(this);
+    //     var $buttonGroup = $this.parents('.button-group');
+    //     var filterGroup = $buttonGroup.attr('data-filter-group');
+    //     filters[ filterGroup ] = $this.attr('data-filter');
+    //     var filterValue = concatValues( filters );
+    //     $grid.isotope({ filter: filterValue });
+    //     updateFilterCounts();
+    // });
+    // /* change is-checked class on buttons*/
+    // $('.button-group').each( function( i, buttonGroup ) {
+    //     var $buttonGroup = $( buttonGroup );
+    //     $buttonGroup.on( 'click', 'button', function() {
+    //         $buttonGroup.find('.is-checked').removeClass('is-checked');
+    //         $( this ).addClass('is-checked');
+    //     });
+    // });
+    // /* flatten object by concatting values */
+    // function concatValues( obj ) {
+    //     var value = '';
+    //     for ( var prop in obj ) {
+    //         value += obj[ prop ];
     //     }
-    // });
+    //     return value;
+    // }
+    // function updateFilterCounts()  {
+    //     var itemElems = $grid.isotope('getFilteredItemElements');
+    //     var $itemElems = $( itemElems );
+    //     $filterButtons.each( function( i, button ) {
+    //         var $button = $( button );
+    //         var filterValue = $button.attr('data-filter');
+    //         if ( !filterValue ) {
+    //             return;
+    //         }
+    //         var count = $itemElems.filter( filterValue ).length;
+    //         $button.find('.filter-count').text( '(' + count +')' );
+    //     });
+    // }
+
+    $('.filters button').click(function(){
+        $('.filters button').removeClass('is-checked');
+        $(this).addClass('is-checked');
+        
+        var data = $(this).attr('data-filter');
+        $grid.isotope({
+          filter: data
+        })
+    });
+      
+    var $grid = $(".grid").isotope({
+        itemSelector: ".all",
+        percentPosition: true,
+        masonry: {
+            columnWidth: ".all"
+        }
+    });
 
  
 
